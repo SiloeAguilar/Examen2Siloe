090f8c19f1126fc850afc69743b8e99f6c5e547a
SiloeAguilar <91304137+SiloeAguilar@users.noreply.github.com>
2023-12-16 10:17:08 -0600

    INTENTO3

--- lib/services/serv.dart
+++ lib/services/serv.dart
@@ -1,44 +1,58 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
+
 class StephenKingService {
-  final Dio dio = Dio();
+  final Dio dio;
+
+  StephenKingService() : dio = Dio();
+
+  StephenKingService.withBaseUrl(String baseUrl) : dio = Dio(BaseOptions(baseUrl: baseUrl));
 
   Future<dynamic> getBooks() async {
     try {
-      final response = await dio.get('https://stephen-king-api.onrender.com/books');
+      final response = await dio.get('/books');
       return response.data;
     } catch (error) {
-      print('Error fetching books: $error');
-      rethrow; // Re-throw the error to let the calling code handle it
+      _handleError('Error fetching books', error);
+      rethrow;
     }
   }
+
   Future<dynamic> getBookByTitle(String title) async {
     try {
-      final response = await dio.get('https://stephen-king-api.onrender.com/books?title=$title');
+      final response = await dio.get('/books', queryParameters: {'title': title});
       return response.data;
     } catch (error) {
-      print('Error fetching book by title: $error');
-      throw error;
+      _handleError('Error fetching book by title', error);
+      return;
     }
   }
 
   Future<dynamic> getBookByID(String id) async {
     try {
-      final response = await dio.get('https://stephen-king-api.onrender.com/books/$id');
+      final response = await dio.get('/books/$id');
       return response.data;
     } catch (error) {
-      print('Error fetching book by ID: $error');
-      throw error;
+      _handleError('Error fetching book by ID', error);
+      rethrow;
     }
   }
 
   Future<dynamic> getAuthor() async {
     try {
-      final response = await dio.get('https://stephen-king-api.onrender.com/author');
+      final response = await dio.get('/author');
       return response.data;
     } catch (error) {
-      print('Error fetching author: $error');
-      throw error;
+      _handleError('Error fetching author', error);
+      rethrow;
+    }
+  }
+
+  void _handleError(String message, dynamic error) {
+    print('$message: $error');
+    if (error is DioError) {
+      // Handle Dio-specific errors here
+      print('DioError details: ${error.response?.data}');
     }
   }
 }
